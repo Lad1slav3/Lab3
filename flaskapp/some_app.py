23da7f15a82db8cb93234d5261087dc54c8e2147
Lad1slav3 <85780007+Lad1slav3@users.noreply.github.com>
2021-06-13 05:25:22 +0300

    Update some_app.py

--- flaskapp/some_app.py
+++ flaskapp/some_app.py
@@ -93,6 +93,7 @@ def twist_image(file_name, choice):
     im = Image.open(file_name)
     fig = plt.figure(figsize=(6, 4))
     ax = fig.add_subplot()
+    image_box=[[]]*1
     im=np.array(image_box[0])/255
     im[100:110,100:110]=(0,1.0,0)
     ax.imshow(im)
